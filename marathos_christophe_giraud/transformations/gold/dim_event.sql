e13af7a782963ad9967f4d84250e4ae775bdc0f5
giraudcgc-cpu <giraudcgc@gmail.com>
2026-06-05 21:48:55 +0000

    Had a conflict and could not commit and push last changes from this morning (like creating validation folder with genie_test notebook)
    Checking if issue is resolved

--- marathos_christophe_giraud/transformations/gold/dim_event.sql
+++ marathos_christophe_giraud/transformations/gold/dim_event.sql
@@ -5,7 +5,7 @@ SELECT
     MAX_BY(event_name, year_of_event) AS event_name,
     MAX_BY(event_type, year_of_event) AS event_type,
     MAX_BY(unit_measure, year_of_event) AS unit_measure,
-    MAX_BY(unit_value, year_of_event) AS unit_value,
-    MAX_BY(event_distance_or_duration, year_of_event) AS event_distance_or_duration
+    MAX_BY(unit_value, year_of_event) AS unit_value
+    -- MAX_BY(event_distance_or_duration, year_of_event) AS event_distance_or_duration
 FROM marathos_cat.silver.marathos_obt
 GROUP BY event_id;
